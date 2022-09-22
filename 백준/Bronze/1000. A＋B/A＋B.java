0504da928e85861fd9f86a803044c03e7c8cf7bb
아현 Ahyeon <aha9654@gmail.com>
2022-09-22 15:33:14 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String num = new Scanner(System.in).nextLine();
+        System.out.println(Integer.parseInt(num.split(" ")[0]) + Integer.parseInt(num.split(" ")[1]));
+    }
+}
