414e87a51597cdf5f5c9fe3b179430cfb8e5ead6
아현 Ahyeon <aha9654@gmail.com>
2022-09-23 00:40:32 +0900

    [Bronze II] Title: 문자열 반복, Time: 144 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int loop = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int loop = Integer.parseInt(reader.readLine());
 
         for (int i = 0; i < loop; i++) {
-            int count = scan.nextInt();
-            String[] word = scan.nextLine().trim().split("");
+            int count = reader.read() - 48;
+            String[] word = reader.readLine().trim().split("");
 
             for (String w : word) {
                 for (int j = 0; j < count; j++) {
