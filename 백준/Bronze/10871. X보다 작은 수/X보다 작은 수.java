e3c8f5f5d79af994d9dba45a6a2b68d0254be3ff
아현 Ahyeon <aha9654@gmail.com>
2022-09-28 12:44:14 +0900

    [Bronze V] Title: X보다 작은 수, Time: 184 ms, Memory: 16180 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] opt = br.readLine().split(" ");
         int N = Integer.parseInt(opt[0]);
         int X = Integer.parseInt(opt[1]);
 
         String[] nums = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            if (Integer.parseInt(nums[i]) < X) System.out.print(nums[i] + " ");
+            if (Integer.parseInt(nums[i]) < X) 
+                bw.write(nums[i] + " ");
         }
+        bw.flush();
     }
 }
