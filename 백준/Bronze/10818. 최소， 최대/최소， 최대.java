1caff1a4f2f7f8553fbcfa606f4e447fe17e07aa
아현 Ahyeon <aha9654@gmail.com>
2022-10-05 02:25:58 +0900

    [Bronze III] Title: 최소, 최대, Time: 528 ms, Memory: 109244 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int min, max;
+
+         min = max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        System.out.printf("%d %d", min, max);
+    }
+}
