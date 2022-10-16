8cf0e574842421b631a8d355e10cfa9a5acd3be9
아현 Ahyeon <aha9654@gmail.com>
2022-10-16 16:55:04 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 152 ms, Memory: 16128 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -2,26 +2,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
-
-    static String fibonacci_dp(int n) {
-        int size = n + 1;
-        int[] f = new int[size];
-        int i = 3;
+    
+    static String fibonacci(Integer n) {
+        int[] f = new int[n + 1];
 
         f[1] = f[2] = 1;
-        for (i = 3; i < size; i++) {
+        for (int i = 3; i <= n; i++) {
             f[i] = f[i - 1] + f[i - 2];
         }
         
-        return f[n] + " " + (i - 3);
+        return f[n] + " " + (n - 2);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        System.out.println(fibonacci_dp(n));
+        System.out.println(fibonacci(n));
     }
 }
