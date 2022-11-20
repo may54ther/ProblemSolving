2675f920dd30b1d38883eb4c7162a9520a07357b
아현 Ahyeon <aha9654@gmail.com>
2022-11-20 20:24:52 +0900

    [Silver I] Title: 정수 삼각형, Time: 484 ms, Memory: 28360 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] triangle = new int[N][N];
+        int[][] sum = new int[N][N];
+        int top, topLeft, topRight, max = 0;
+
+        for (int x = 0; x < N; x++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int y = 0; y < N; y++) {
+                int y2 = y;
+
+                if (st.hasMoreTokens()) {
+                    triangle[x][y] = Integer.parseInt(st.nextToken());
+                } else triangle[x][y] = -1;
+
+                sum[x][y] = triangle[x][y];
+                if (x > 0 && triangle[x][y] > -1) {
+                    if (y > 0) y2 = y - 1;
+                    sum[x][y] += Math.max(sum[x - 1][y2], sum[x - 1][y]);
+                }
+
+                for (int n : sum[N - 1]) {
+                    max = Math.max(n, max);
+                }
+
+
+            }
+        }
+        System.out.println(max);
+    }
+}
