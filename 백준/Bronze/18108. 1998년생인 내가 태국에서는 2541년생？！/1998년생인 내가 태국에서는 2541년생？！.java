7bd4f904489916aadf2fa54c3fcf2e5f54e74df6
아현 Ahyeon <aha9654@gmail.com>
2023-04-12 19:17:18 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int y = new Scanner(System.in).nextInt();
+
+        System.out.println(y - 543);
+    }
+}
