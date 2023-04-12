e5ed317f901aa040c6e46744d57de7328a32786f
아현 Ahyeon <aha9654@gmail.com>
2023-04-12 19:17:29 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = new Scanner(System.in).nextLine();
+
+        System.out.println(str + "??!");
+    }
+}
