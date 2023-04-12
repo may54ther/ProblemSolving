ab6ae8f65afef5a4f49a8367460bf0ab3102fa63
아현 Ahyeon <aha9654@gmail.com>
2023-04-12 19:15:56 +0900

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,24 +1,26 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String str;
-        int a, b;
-        StringTokenizer st;
+        String[] arr = new String[2];
+        int sum;
 
-        while ((str = br.readLine()) != null) {
-            st = new StringTokenizer(str);
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            bw.write(a + b + "\n");
+        while (true) {
+            try {
+                arr = br.readLine().split(" ");
+                sum = Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]);
+                bw.write(String.valueOf(sum));
+                bw.newLine();
+                bw.flush();
+            } catch (Exception e) {
+                break;
+            }
         }
-        
-        br.close();
-        bw.flush();
-        bw.close();
     }
 }
