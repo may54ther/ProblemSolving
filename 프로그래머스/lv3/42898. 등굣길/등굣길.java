ae39cb264a4d0aa32d473a1c0e8b03d136ee2ad6
아현 Ahyeon <aha9654@gmail.com>
2023-04-14 21:25:00 +0900

    [level 3] Title: 등굣길, Time: 0.88 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.java
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.java
@@ -1,34 +1,31 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int m, int n, int[][] puddles) {
         
         int[][] map = new int[n][m];
-        
         for(int[] puddle : puddles) {
             int x = puddle[1]-1;
             int y = puddle[0]-1;
-            
             map[x][y] = -1;
         }
-       
+
         for(int i = 1; i < n; i++){
             if(map[i][0] > -1) {
                 map[i][0] = map[i-1][0] > -1 ? 1 : -1;
             }
-         
+
             for(int j = 1; j < m; j++){
                 if(map[i][j] > -1) {
                     if(map[0][j] > -1) {
                       map[0][j] = map[0][j-1] > -1 ? 1 : -1;
                     }
                     map[i][j] = (Math.max(0, map[i][j-1]) + Math.max(0, map[i-1][j])) % 1000000007;
-                    
+
                 }
             }
         }
-        
+
         return map[n-1][m-1];
     }
 }
