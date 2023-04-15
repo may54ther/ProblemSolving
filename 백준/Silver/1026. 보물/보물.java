2818b7868c931a942e65eea33fe6605259abcbdc
아현 Ahyeon <aha9654@gmail.com>
2023-04-15 16:47:19 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,28 +1,32 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Main {
      public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String[][] arr = new String[2][n];
-        arr[0] = br.readLine().split(" ");
-        arr[1] = br.readLine().split(" ");
-
+        String[] A = br.readLine().split(" ");
+        String[] B = br.readLine().split(" ");
+       
         int sum = 0;
-        int[] A = Arrays.stream(arr[0])
-                        .mapToInt(Integer::parseInt)
-                        .sorted()
-                        .toArray();
-        int[] B = Arrays.stream(arr[1])
-                        .mapToInt(Integer::parseInt)
-                        .sorted()
-                        .toArray();
+        Arrays.sort(A, new Comparator<String>() {
+            @Override
+            public int compare(String x, String y) {
+                return Integer.compare(Integer.parseInt(x), Integer.parseInt(y));
+            }
+        });
+        Arrays.sort(B, new Comparator<String>() {
+            @Override
+            public int compare(String x, String y) {
+                return Integer.compare(Integer.parseInt(y), Integer.parseInt(x));
+            }
+        });
 
         for (int i = 0; i < n; i++) {
-            sum += A[i] * B[(n - 1) - i];
+            sum += Integer.parseInt(A[i]) * Integer.parseInt(B[i]);
         }
 
         System.out.println(sum);
