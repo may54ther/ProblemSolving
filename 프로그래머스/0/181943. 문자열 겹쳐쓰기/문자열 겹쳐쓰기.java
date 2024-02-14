8977b7d89f896545ab2df9818e47146a48db0e18
아현 Ahyeon <aha9654@gmail.com>
2024-02-14 18:21:58 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 17.83 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public static String solution(String my_string, String overwrite_string, int s) {
+        return my_string.substring(0, s) +
+               overwrite_string +
+               my_string.substring((s + overwrite_string.length()), my_string.length()) ;
+    }
+}
