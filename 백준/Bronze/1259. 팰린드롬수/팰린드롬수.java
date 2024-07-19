acf4c956ac7408f1a2ac3f27034cd479672a8464
아현 Ahyeon <aha9654@gmail.com>
2024-07-19 19:01:44 +0900

    [Bronze I] Title: 팰린드롬수, Time: 120 ms, Memory: 13944 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,33 +1,35 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = "";
+        String line;
 
-        while (!(input = br.readLine()).equals("0")) {
-
-            if (input.charAt(0) == '0') {
-                System.out.println("no");
-                continue;
+        while (true) {
+            line = br.readLine();
+            if ("0".equals(line)) {
+                break;
             }
 
-            int length = input.length();
-            int middle = Math.floorDiv(length, 2);
+            int size = line.length();
+            char[] chars = line.toCharArray();
+            boolean check = true;
 
-            String str1 = input.substring(0, middle);
-            String str2 = new StringBuilder(input.substring(length - middle)).reverse().toString();
-
-            if (str1.equals(str2)) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
+            if (chars[0] == '0')
+                check = false;
+            else {
+                for (int i = 0; i < Math.floorDiv(size, 2); i++) {
+                    if (chars[i] != chars[size - 1 - i]) {
+                        check = false;
+                        break;
+                    }
+                }
             }
+
+            System.out.println(!check ? "no" : "yes");
         }
     }
 }
