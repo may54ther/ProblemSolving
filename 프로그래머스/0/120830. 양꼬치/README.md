12ec2d9a13f4beff0aec84670d40f066b03dd621
ahyeon <aha9654@gmail.com>
2025-02-23 21:13:11 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/README.md
+++ 프로그래머스/0/120830. 양꼬치/README.md
@@ -1,10 +1,10 @@
 # [level 0] 양꼬치 - 120830 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120830) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120830?language=java) 
 
 ### 성능 요약
 
-메모리: 86.3 MB, 시간: 0.04 ms
+메모리: 73.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 23일 20:54:30
+2025년 02월 23일 21:13:05
 
 ### 문제 설명
 
--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,8 +1,9 @@
 class Solution {
-    private final int FOOD_PRICE = 12000;
+    
+    private final int LAMB_SKEWERS_PRICE = 12000;
     private final int DRINK_PRICE = 2000;
     
     public int solution(int n, int k) {
-        return (n * FOOD_PRICE) + ((n >= 10 ? k - Math.round(n / 10) : k) * DRINK_PRICE);
+        return (n * LAMB_SKEWERS_PRICE) + ((k - (n / 10)) * DRINK_PRICE);
     }
 }
