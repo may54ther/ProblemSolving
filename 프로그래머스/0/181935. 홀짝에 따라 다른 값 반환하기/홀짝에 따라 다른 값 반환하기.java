b4da6503f79e098b648d67c0e39a36f12633d66f
ahyeon <aha9654@gmail.com>
2025-06-01 20:46:08 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.06 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while (n > 0) {
+            answer += n % 2 == 0 ? Math.pow(n, 2) : n;
+            n -= 2;
+        }
+        return answer;
+    }
+}
