f8f175a9fda67fc9a2612090872cba6789c2a9ba
ahyeon <aha9654@gmail.com>
2026-02-18 18:00:59 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,19 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int currentHp = hp;
-        int attacks = 0;
-
-        if (currentHp >= 5) {
-            int count = (int) currentHp / 5;
-            currentHp -= (count * 5);
-            attacks += count;
-        }
-        if (currentHp >= 3) {
-            int count = (int) currentHp / 3;
-            currentHp -= (count * 3);
-            attacks += count;
-        }
-        
-        return attacks + currentHp;
+        return (hp / 5) + (hp % 5 / 3) + (hp % 5 % 3);
     }
 }
