f2a18b56567674b5b3fb3d77462bdb1b0e438a97
ahyeon <aha9654@gmail.com>
2026-02-18 12:55:54 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] solution(int[] array) {
         int[] answer = { array[0], 0 };
+        
         for (int i = 1; i < array.length; i++) {
             if (answer[0] < array[i]) {
                 answer[0] = array[i];
