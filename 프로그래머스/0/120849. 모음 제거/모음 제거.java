b60b0266d280e628cbc237166ba4c8f8d2475d0c
ahyeon <aha9654@gmail.com>
2026-02-18 17:27:26 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,13 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder builder = new StringBuilder();
-        
-        for (char c : my_string.toCharArray()) {
-            if ("aeiou".indexOf(c) == -1) {
-                builder.append(String.valueOf(c));
-            }
-        }
-        
-        return builder.toString();
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
